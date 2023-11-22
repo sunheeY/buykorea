001abb12d60f848361eba851cc8c00f4318293b4
sunghee Y <cashmeremist@naver.com>
2023-11-22 10:49:16 +0900

    헤더 작업중

--- 0_html/js/mobile/ui_common.js
+++ 0_html/js/mobile/ui_common.js
@@ -49,7 +49,7 @@ function preLoad() {
   }
 
   // 좋아요
-  $('.btn-like').click( function() {
+  $('.btn-like, .bk-icon-like').click( function() {
     $(this).toggleClass('is-active');
   })
 }
@@ -769,3 +769,25 @@ $(document).ready(function(e) {
     });
   }
   */
+
+  function scrollCall() {
+    var lastScrollTop = $(window).scrollTop();
+    $(window).scroll(function() {
+      var st = $(this).scrollTop();
+      var hideHeight = $('.header-comm').outerHeight() + 1;  
+
+      if (st <= 1 || st < lastScrollTop) {
+        $('.ui-header').css('transform', 'translateY(0)');
+        // console.log('위로');
+      } else {
+        $('.ui-header').css('transform', 'translateY(-' + hideHeight + 'px)');
+        // console.log('아래로');
+      }
+      lastScrollTop = st;
+    });
+  }
+
+  $(document).ready(function () {
+    scrollCall();
+    $(window).scroll(scrollCall)
+  });
